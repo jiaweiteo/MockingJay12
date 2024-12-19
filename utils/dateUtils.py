d3c4be249d7c3fb1cb0791ccf5a9833fb4af2f00
isocolon <70964355+isocolon@users.noreply.github.com>
2024-12-19 11:32:32 +0800

    revert seconds

--- utils/dateUtils.py
+++ utils/dateUtils.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def date_string_to_date_obj(date_string):
     return datetime.strptime(date_string, "%Y-%m-%d")
 
-def time_string_to_datetime_obj(time_string, time_format="%H:%M"):
+def time_string_to_datetime_obj(time_string, time_format="%H:%M:%S"):
     return datetime.strptime(time_string, time_format)
 
 def format_date(date_string):
@@ -11,13 +11,13 @@ def format_date(date_string):
     formatted_date = date_obj.strftime("%b %d, %Y")
     return formatted_date
 
-def combine_date_and_time(date_str, time_str, date_format="%Y-%m-%d", time_format="%H:%M"):
+def combine_date_and_time(date_str, time_str, date_format="%Y-%m-%d", time_format="%H:%M:%S"):
     # Parse the date and time strings
     date_part = datetime.strptime(date_str, date_format)
     time_part = datetime.strptime(time_str, time_format).time()
 
     # Combine date and time
-    return datetime.combine(date_part, time_part).strftime("%Y-%m-%d %H:%M")
+    return datetime.combine(date_part, time_part).strftime("%Y-%m-%d %H:%M:%S")
 
 def datetime_string_to_date_and_time_object(date_time_string):
     datetime_object = datetime.fromisoformat(date_time_string)
